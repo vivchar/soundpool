e607e7a20afa38adb93cbfba6a9d98f90232125f
Vivchar Vitaly <vvv@tomsksoft.com>
2020-04-27 21:32:57 +0700

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,18 +1,22 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "9b8ff5f1a34146a1530ada2a21fba603",
-"/": "9b8ff5f1a34146a1530ada2a21fba603",
-"main.dart.js": "f7c3c35d92602e804c27e12240ca8f78",
-"c-c-1.mp3": "4b027868d0652aa597bd7462e70aeefc",
+  "index.html": "9572557cd8883fe1c8a3e7ec22bb0b77",
+"/": "9572557cd8883fe1c8a3e7ec22bb0b77",
+"main.dart.js": "153c4aa390d55ff6a5a7a736bcebb46b",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"manifest.json": "bdced5666247243fe5a358f2abfe3546",
 "assets/LICENSE": "d36b9e32e2e245f0a278263266998f11",
-"assets/AssetManifest.json": "482935e634af5e4e463d9366ed440225",
-"assets/sounds/dices.m4a": "1f3f38c91b4d12b923d5e7fa2197a269",
-"assets/sounds/van-sliding-door-daniel_simon.wav": "0a5f8e468c60014d7d776de0ce1cc69c",
-"assets/sounds/do-you-like-it.wav": "48ebe7e117a89596fe0f278cd1c7ca43",
+"assets/AssetManifest.json": "6a0a6979070393a7fa46f171f1a1bf2a",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/assets/sounds/sound_bell_boxing_1.wav": "0faec93f7212dafc7a5afe33aeb78965",
+"assets/assets/sounds/sound_bell_boxing_3.wav": "2efae6d85a3bb90a6437ab1cb0f3b94f",
+"assets/assets/sounds/sound_click.wav": "94f0f74edc3195ab742018bc5928c5db",
+"assets/assets/sounds/sound_whistle_01.wav": "fec36d1963c9bf3043086861e3699bdf"
 };
 
 self.addEventListener('activate', function (event) {
